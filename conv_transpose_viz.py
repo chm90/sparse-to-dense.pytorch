40c3eaac31160f7425932ac423a7a195edb7db71
Christoffer Möckelind <christoffer.mockelind@live.se>
2018-08-11 18:41:54 +0200

    wip

--- conv_transpose_viz.py
+++ conv_transpose_viz.py
@@ -2,11 +2,13 @@ from argparse import ArgumentParser
 import torch
 from torch import nn
 from matplotlib import pyplot as plt
+from matplotlib import gridspec
 from torch.autograd import Variable
+from math import ceil, floor
+from typing import Iterator
 parser = ArgumentParser(
     description=
     "plots images showing the effect of different transposed convolutions")
-
 
 def plot_convt_output(X,
                       ax,
@@ -27,6 +29,9 @@ def plot_convt_output(X,
     ax.set_ylabel(f"{Y.shape[3]} px")
     ax.set_xticklabels([])
     ax.set_yticklabels([])
+
+
+
 
 
 def main():
@@ -54,6 +59,29 @@ def main():
         "padding": 1,
         "output_padding": 1
     }]
+    
+    def gridspecsi(n_params : int) -> Iterator[gridspec.Gridspec]:
+        cols = 3
+        rows = ceil(n_params + 1 / cols)
+        last_row_cols = cols - (cols * rows - n_params)
+        gs = gridspec.Gridspec(rows,cols * 2)
+        for row in range(rows - 1):
+            for col in range(cols):
+                col_grid = col * 2
+                yield gs[row,col_grid:col_grid + 2]
+        if last_row_cols == 1:
+            col_ = floor(cols / 2)
+            yield gs[col_:col_ + 2]
+        elif last_row_cols == 2:
+            col1_ = slice(1,3)
+            col2_ = slice(3,5)
+            yield gs[col1_: col1_ + 2]
+            yield gs[col2_: col2_ + 2]
+        elif last_row_cols == 3:
+            raise NotImplementedError("Implement meeeee")
+
+    fig = plt.figure()
+    ax1 = plt.subplot()
     fig, [ax1, *axes] = plt.subplots(1, len(params) + 1)
     ax1.imshow(X[0, 0, ...].data)
     ax1.set_title("X")
@@ -61,6 +89,7 @@ def main():
     ax1.set_ylabel(f"{X.shape[3]} px")
     ax1.set_xticklabels([])
     ax1.set_yticklabels([])
+    gridspecsi()
 
     for ax, conv_params in zip(axes, params):
         plot_convt_output(X, ax, **conv_params)
